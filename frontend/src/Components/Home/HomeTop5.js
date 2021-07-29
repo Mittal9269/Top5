7336d5b84bc399ad376b3e62e263ef9c9ffa9ff6
Mittal9269 <190030026@iitsh.ac.in>
2021-07-29 18:01:22 +0530

    first_commit

--- frontend/src/Components/Home/HomeTop5.js
+++ frontend/src/Components/Home/HomeTop5.js
@@ -1,7 +1,7 @@
-import { useState , useEffect } from "react"
+import { useState, useEffect } from "react"
 import HomeBooks from "./HomeBooks";
 
-export default function HomeTop5(){
+export default function HomeTop5() {
     const [data, setData] = useState();
     const [Category, setCategory] = useState([]);
     const [handleSplit, setHandleSplit] = useState({});
@@ -17,23 +17,54 @@ export default function HomeTop5(){
             .then(fecthnow => {
 
                 // setComment(fecthnow);
+                // console.log(fecthnow)
                 let temp_arr = [];
                 let comment_object = {};
                 fecthnow.forEach(element => {
-                    if(!temp_arr.includes(element.Category)){
-                        comment_object[element] = [];
-                        temp_arr.push(element)
+                    if (!temp_arr.includes(element.Category)) {
+                        comment_object[element.Category] = [];
+                        temp_arr.push(element.Category)
                     }
                 });
                 setCategory(temp_arr);
                 let arr = [];
                 fecthnow.forEach(element => {
                     // if(userInfo.category.includes(element.Category)){
-                        arr.push(element.BookId);
+                    arr.push(element.BookId);
                     // console.log(element.Category);
                     let temp = [...comment_object[element.Category], element]
                     comment_object[element.Category] = temp;
                     // } 
+                });
+                
+                temp_arr.forEach(element => {
+                    let books = comment_object[element];
+                    console.log(books)
+                    let result = {};
+                    let p = [];
+                    for (let i = 0; i < books.length; ++i) {
+                        if(!p.includes(books[i].BookId)){
+                            p.push(books[i].BookId)
+                            result[books[i]] = 0;
+                        }
+                        // if (!result[books[i].BookId])
+                        ++result[books[i]];
+                    }
+                    console.log(result)
+                    // let sortable = Object.fromEntries(
+                    //     Object.entries(result).sort(([,a],[,b]) => a-b)
+                    //     );
+                    // let bookKey = Object.keys(sortable);
+                    // bookKey.reverse()
+                    // let new_array;
+                    // if(bookKey.length > 5){
+                    //     new_array = bookKey.slice(0, 5)
+                    // }
+                    // else{
+                    //     new_array = bookKey;
+                    // }
+                    // console.log(new_array)
+                    // comment_object[element] = new_array;
                 });
 
                 setHandleSplit(comment_object);
@@ -44,51 +75,40 @@ export default function HomeTop5(){
             }).catch(err => console.log(err))
     }, [])
 
-    const popUpRemvoe = (bol, identity) => {
-
-        // setIden(identity)
-
-        // if (comment[iden] != undefined && comment[iden] != null) {
-        //     setIdRating(comment[iden].Rating)
-        //     setIdreview(comment[iden].Review)
-        // }
-        // setIsOpen(bol);
-    }
 
     return (
         <>
             <hr />
             {Category !== undefined && Category.length !== 0 && (Category.map((value, index) => {
                 return (
-                <div>
-                    {(handleSplit[value] !== undefined &&  handleSplit[value].length!== 0) ? <h3 className="text-center">Your Top books in {value}</h3> : null}
-                    <div className="container-fluid mb-5">
-                        <div className="row">
-                            <div className="clo-10 mx-auto">
-                                <div className="row">
-                                    <div className="col-10 mx-auto">
-                                        <div className="row gy-4">
-                                            
-                                            {console.log(handleSplit[value])}
-                                            {handleSplit[value] !== undefined && handleSplit[value].length !== 0 && (handleSplit[value].map((val, ind) => {
-                                                {console.log(val)}
-                                                return (
-                                                    <HomeBooks
-                                                        key={ind}
-                                                        indenity={ind}
-                                                        commentAll={val}
-                                                        HandleRemove={popUpRemvoe}
-                                                    />  
-                                                )
-                                                
-                                            }))}
+                    <div>
+                        {(handleSplit[value] !== undefined && handleSplit[value].length !== 0) ? <h3 className="text-center"> Top books in {value}</h3> : null}
+                        <div className="container-fluid mb-5">
+                            <div className="row">
+                                <div className="clo-10 mx-auto">
+                                    <div className="row">
+                                        <div className="col-10 mx-auto">
+                                            <div className="row gy-4">
+
+                                                {/* {console.log(handleSplit[value])} */}
+                                                {handleSplit[value] !== undefined && handleSplit[value].length !== 0 && (handleSplit[value].map((val, ind) => {
+                                                    {/* { console.log(val) } */}
+                                                    return (
+                                                        <HomeBooks
+                                                            key={ind}
+                                                            indenity={ind}
+                                                            commentAll={val}
+                                                        />
+                                                    )
+
+                                                }))}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>)
+                    </div>)
             }))}
         </>
     )
